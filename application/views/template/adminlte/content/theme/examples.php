4943434ed37af9428a55568aa78c4390f2f0b188
Hanief <haniefhan@gmail.com>
2018-09-06 16:12:25 +0700

    theme documentation error fixed

--- application/views/template/adminlte/content/theme/examples.php
+++ application/views/template/adminlte/content/theme/examples.php
@@ -3,7 +3,7 @@
 		<div class="panel panel-default">
 			<div class="panel-heading">Theme Documentation</div>
 			<div class="panel-body">
-				<iframe src="<?php echo asset_admin_url().$data['example'] ?>" style="width:100%;height:400px"></iframe>
+				<iframe src="<?php echo base_url().'assets/'.$data['url'].$data['example'] ?>" style="width:100%;height:400px"></iframe>
 				<button type="button" class="btn btn-primary" onclick="goBack()">Back</button>
 				<a href="<?php echo base_url_admin().'theme' ?>" class="btn btn-default">Back to Theme</a>
 			</div>
