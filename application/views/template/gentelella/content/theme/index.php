3918a0304eb17c56f9657a94703e5c75fbaaefb1
Hanief <haniefhan@gmail.com>
2018-09-06 22:07:16 +0700

    theme management :: menambahkan jika terdapat public theme; theme adminlte :: mengubah nama Admin LTE menjadi sesuai dengan nama sistem di database

--- application/views/template/gentelella/content/theme/index.php
+++ application/views/template/gentelella/content/theme/index.php
@@ -1,4 +1,27 @@
 <div class="row" id="theme">
+	<?php if(count($public) > 0){ ?>
+		<div class="col-lg-12">
+			<h3>Public Template</h3>
+		</div>
+		<?php foreach ($public as $data) {?>
+			<div class="col-lg-3">
+				<div class="panel panel-default">
+					<div class="panel-heading"><?php echo ucwords(str_replace('_', ' ', $data['name'])); ?></div>
+					<div class="panel-body">
+						<img src="<?php echo base_url().'assets/'.$data['url'].'screenshot-home.png' ?>" class="screenshot"  style="max-height: 100px;">
+					</div>
+					<div class="panel-footer">
+						<a href="<?php echo base_url_admin() ?>theme/index?type=examples&id=<?php echo $data['id'] ?>" class="btn btn-default">Doc</a>
+						<?php if($data['url'] != $this->session->userdata('template_use')){ ?>
+							<a href="<?php echo base_url_admin().'theme/update?id='.$data['id'].'&type='.$data['type'] ?>" class="btn btn-primary">Activate</a>
+						<?php }else{ ?>
+							<span class="btn btn-success" disabled="disabled">Activated</span>
+						<?php } ?>
+					</div>
+				</div>
+			</div>
+		<?php } ?>
+	<?php } ?>
 	<div class="col-lg-12">
 		<h3>Admin Template</h3>
 	</div>
