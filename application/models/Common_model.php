58b871e36205974a36114105c01108044ab74c66
Hanief <haniefhan@gmail.com>
2018-09-12 10:04:24 +0700

    common :: example of controller and model using common

---
+++ application/models/Common_model.php
@@ -0,0 +1,181 @@
+<?php
+class Common_model extends MY_Model {
+	protected $_table 		= 'common';
+	public $primary_key = 'com_id';
+
+	public $table_field = array(
+		0 => array(
+			// table param
+			'name' 			=> 'No',
+			'table_index'	=> 'com_id',
+			'style' 		=> '',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> false,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> false,
+			'type' 			=> 'hidden', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> false,
+			'maxlength' 	=> '',
+			// print param
+			'in_print'		=> true,
+		),
+		1 => array(
+			// table param
+			'name' 			=> 'Name',
+			'table_index'	=> 'com_name',
+			'style' 		=> '',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> true,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> true,
+			'type' 			=> 'text', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> true,
+			'maxlength' 	=> '255',
+			// print param
+			'in_print'		=> true,
+		),
+		2 => array(
+			// table param
+			'name' 			=> 'Date 1',
+			'table_index'	=> 'com_date1',
+			'style' 		=> '',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> true,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> true,
+			'type' 			=> 'date', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> true,
+			'maxlength' 	=> '',
+			// print param
+			'in_print'		=> true,
+		),
+		3 => array(
+			// table param
+			'name' 			=> 'Date 2',
+			'table_index'	=> 'com_date2',
+			'style' 		=> '',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> true,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> true,
+			'type' 			=> 'datepicker', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> true,
+			'maxlength' 	=> '',
+			// print param
+			'in_print'		=> true,
+		),
+		4 => array(
+			// table param
+			'name' 			=> 'Numeric',
+			'table_index'	=> 'com_numeric',
+			'style' 		=> '',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> true,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> true,
+			'type' 			=> 'numeric', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> true,
+			'maxlength' 	=> '',
+			// print param
+			'in_print'		=> true,
+		),
+		5 => array(
+			// table param
+			'name' 			=> 'Money',
+			'table_index'	=> 'com_money',
+			'style' 		=> '',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> true,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> true,
+			'type' 			=> 'money', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> true,
+			'maxlength' 	=> '',
+			// print param
+			'in_print'		=> true,
+		),
+		6 => array(
+			// table param
+			'name' 			=> 'Text',
+			'table_index'	=> 'com_text',
+			'style' 		=> '',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> true,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> true,
+			'type' 			=> 'textarea', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> true,
+			'maxlength' 	=> '',
+			// print param
+			'in_print'		=> true,
+		),
+		7 => array(
+			// table param
+			'name' 			=> 'Action',
+			'table_index'	=> 'com_id',
+			'style' 		=> '', // 'width:15%;',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> false,
+			'sortable' 		=> false,
+			// form param
+			'in_form' 		=> false,
+			'type' 			=> 'hidden', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> false,
+			'maxlength' 	=> '',
+			// print param
+			'in_print'		=> false,
+		),
+	);
+
+	public function reformat_post_to_sql($data = array()){
+		$table_field = $this->table_field;
+
+		foreach ($table_field as $i => $tf) {
+			if($tf['in_form'] == true){
+				if($tf['type'] == 'date' or $tf['type'] == 'datepicker') $data[$tf['table_index']] = $this->reformat_date($data[$tf['table_index']]);
+				elseif($tf['type'] == 'numeric' or $tf['type'] == 'money') $data[$tf['table_index']] = $this->reformat_numeric($data[$tf['table_index']]);
+			}
+		}
+
+		return $data;
+	}
+
+	protected function reformat_date($date = ''){
+		$d = explode('/', $date);
+		return $d[2].'-'.$d[1].'-'.$d[0];
+	}
+
+	protected function reformat_numeric($numeric = 0){
+		// indonesian format
+		if(strpos($numeric, '.')){
+			$numeric = str_replace('.', '', $numeric);
+		}elseif(strpos($numeric, ',')){
+			$numeric = str_replace(',', '.', $numeric);
+		}
+		return $numeric;
+	}
+}
+?>
