a1fe2115089c787d3d9657683faa9f7383bbd364
Hanief <haniefhan@gmail.com>
2018-09-18 15:54:59 +0700

    theme :: add open in new tab

--- application/views/template/adminlte/content/theme/examples.php
+++ application/views/template/adminlte/content/theme/examples.php
@@ -6,6 +6,7 @@
 				<iframe src="<?php echo base_url().'assets/'.$data['url'].$data['example'] ?>" style="width:100%;height:400px"></iframe>
 				<button type="button" class="btn btn-primary" onclick="goBack()">Back</button>
 				<a href="<?php echo base_url_admin().'theme' ?>" class="btn btn-default">Back to Theme</a>
+				<a href="<?php echo base_url().'assets/'.$data['url'].$data['example'] ?>" class="btn btn-danger pull-right" target="_blank">Open in New Tab</a>
 			</div>
 		</div>
 	</div>
--- application/views/template/basic/content/theme/examples.php
+++ application/views/template/basic/content/theme/examples.php
@@ -3,9 +3,10 @@
 		<div class="panel panel-default">
 			<div class="panel-heading">Theme Documentation</div>
 			<div class="panel-body">
-				<iframe src="<?php echo asset_admin_url().$data['example'] ?>" style="width:100%;height:400px"></iframe>
+				<iframe src="<?php echo base_url().'assets/'.$data['url'].$data['example'] ?>" style="width:100%;height:400px"></iframe>
 				<button type="button" class="btn btn-primary" onclick="goBack()">Back</button>
 				<a href="<?php echo base_url_admin().'theme' ?>" class="btn btn-default">Back to Theme</a>
+				<a href="<?php echo base_url().'assets/'.$data['url'].$data['example'] ?>" class="btn btn-danger pull-right" target="_blank">Open in New Tab</a>
 			</div>
 		</div>
 	</div>
