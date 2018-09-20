1dc375620e207f8178650899c07776e6347ccbe3
Hanief <haniefhan@gmail.com>
2018-09-20 14:54:01 +0700

    user :: user use common controller and model

--- application/controllers/admin/User.php
+++ application/controllers/admin/User.php
@@ -5,47 +5,72 @@ class User extends Admin_Controller implements ControllerInterface{
 		parent::__construct();
 		$this->load->model("User_model", 'user');
 		$this->title = 'User';
+		$this->controller = 'user';
 		$this->redirect_url = base_url_admin().'user';
-		$this->lang->load('user', $this->session->userdata('lang'));
 	}
 
 	function index(){
+		$data = array();
 		$data['title']		= $this->title;
-		$data['content']	= 'content/user/index';
-		$this->user->select('user.*, group.name as group_name');
-		$this->user->join('group');
-		$this->user->order_by('user.id','DESC');
-		$data['datas']		= array();// $this->user->get_all();
+		$data['controller']	= $this->controller;
+		$data['datas']		= array();
 		$data['datatable']	= base_url_admin().'user/datatable';
+
+		// because it's simple index and form, use user form
+		$data['table_field'] = $this->user->table_field;
+		$data['content']	= 'content/common/index';
 
 		$this->template($data);
 	}
 
 	// add Page by form
 	function add(){
+		$data = array();
 		$data['title']		= 'Add New '.$this->title;
+		$data['controller']	= $this->controller;
 		$data['state']		= 'add';
-		$data['content']	= 'content/user/form';
-		
-		$this->load->model('Group_model', 'group');
-		$this->group->order_by('id', 'DESC');
-		$data['group']		= $this->group->get_all();
 
+		// because it's simple index and form, use user form
+		$data['table_field'] = $this->user->table_field;
+		foreach ($data['table_field'] as $i => $tf) {
+			if($tf['type'] == 'select'){
+				$this->load->model($tf['value'][0]);
+				$data['table_field'][$i]['value'] = $this->$tf['value'][0]->populate_select($tf['value'][1], $tf['value'][2], $tf['value'][3]);
+			}elseif($tf['type'] == 'select-year'){
+				$this->load->model($tf['value'][0]);
+				$data['table_field'][$i]['value'] = $this->$tf['value'][0]->populate_select_year($tf['value'][1], $tf['value'][2], $tf['value'][3]);
+			}
+		}
+		$data['primary_key'] = $this->user->primary_key;
+		$data['content']	= 'content/common/form';
 		$this->template($data);
 	}
 
 	// edit Page by form
 	function edit(){
 		$id = $this->input->get('id');
+		$data = array();
 		$data['title']		= 'Edit '.$this->title;
+		$data['controller']	= $this->controller;
 		$data['state']		= 'edit';
-		$data['content']	= 'content/user/form';
-		$data['datas'] 		= $this->user->get($id);
 		$data['id'] 		= $id;
-		
-		$this->load->model('Group_model', 'group');
-		$this->group->order_by('id', 'DESC');
-		$data['group']		= $this->group->get_all();
+
+		// because it's simple index and form, use user form
+		$data['table_field'] = $this->user->table_field;
+		foreach ($data['table_field'] as $i => $tf) {
+			if($tf['type'] == 'select'){
+				$this->load->model($tf['value'][0]);
+				$data['table_field'][$i]['value'] = $this->$tf['value'][0]->populate_select($tf['value'][1], $tf['value'][2], $tf['value'][3]);
+			}elseif($tf['type'] == 'select-year'){
+				$this->load->model($tf['value'][0]);
+				$data['table_field'][$i]['value'] = $this->$tf['value'][0]->populate_select_year($tf['value'][1], $tf['value'][2], $tf['value'][3]);
+			}
+		}
+		$data['primary_key'] = $this->user->primary_key;
+		$data['content']	= 'content/common/form';
+		$data['datas']      = $this->user->get($id);
+
+		$data['datas'] = $this->user->reformat_sql_to_form($data['datas']);
 
 		$this->template($data);
 	}
@@ -53,18 +78,30 @@ class User extends Admin_Controller implements ControllerInterface{
 	function insert(){
 		$this->db->trans_start();
 		$data = $this->input->post();
-		$data['password'] = genpass($data['password']);
+
+		$data = $this->user->reformat_post_to_sql($data);
+
+		// confirm password
+		if($data['password'] != $data['confirm_password']){
+			$this->session->set_flashdata('notif_status', false);
+			$this->session->set_flashdata('notif_msg', 'Password and Confirm Password must be same!');
+			$this->db->trans_complete();
+			redirect($this->redirect_url);
+		}elseif($data['password'] != ''){
+			$data['password'] = genpass($data['password']);
+			unset($data['confirm_password']);
+		}
+		// end of confirm password
 
 		if($this->user->insert($data)){
 			$this->session->set_flashdata('notif_status', true);
-			$this->session->set_flashdata('notif_msg', 'Add new user success');
+			$this->session->set_flashdata('notif_msg', 'Add new '.$this->title.' success');
 		}else{
 			$this->session->set_flashdata('notif_status', false);
-			$this->session->set_flashdata('notif_msg', 'Add new user failed');
+			$this->session->set_flashdata('notif_msg', 'Add new '.$this->title.' failed');
 		}
 
 		$this->db->trans_complete();
-
 		redirect($this->redirect_url);
 	}
 
@@ -72,16 +109,31 @@ class User extends Admin_Controller implements ControllerInterface{
 		$this->db->trans_start();
 		$id = $this->input->get('id');
 		$data = $this->input->post();
-		if($data['password'] != '') $data['password'] = genpass($data['password']);
-		else unset($data['password']);
 
-		$id = $this->user->update($id, $data);
-		if($id !== false){
+		$data = $this->user->reformat_post_to_sql($data);
+
+		// confirm password
+		if($data['password'] != '' AND ($data['password'] != $data['confirm_password'])){
+			$this->session->set_flashdata('notif_status', false);
+			$this->session->set_flashdata('notif_msg', 'Password and Confirm Password must be same!');
+			$this->db->trans_complete();
+			redirect($this->redirect_url);
+		}elseif($data['password'] != ''){
+			$data['password'] = genpass($data['password']);
+			unset($data['confirm_password']);
+		}else{
+			unset($data['password']);
+			unset($data['confirm_password']);
+		}
+		// end of confirm password
+
+		$st = $this->user->update($id, $data);
+		if($st !== false){
 			$this->session->set_flashdata('notif_status', true);
-			$this->session->set_flashdata('notif_msg', 'Update user success');
+			$this->session->set_flashdata('notif_msg', 'Update '.$this->title.' success');
 		}else{
 			$this->session->set_flashdata('notif_status', false);
-			$this->session->set_flashdata('notif_msg', 'Update user failed');
+			$this->session->set_flashdata('notif_msg', 'Update '.$this->title.' failed');
 		}
 		$this->db->trans_complete();
 
@@ -90,30 +142,164 @@ class User extends Admin_Controller implements ControllerInterface{
 
 	function delete(){
 		$id = $this->input->get('id');
+
 		if($this->user->delete($id)){
 			$this->session->set_flashdata('notif_status', true);
-			$this->session->set_flashdata('notif_msg', 'Delete user success');
+			$this->session->set_flashdata('notif_msg', 'Delete '.$this->title.' success');
 		}else{
 			$this->session->set_flashdata('notif_status', false);
-			$this->session->set_flashdata('notif_msg', 'Delete user failed');
+			$this->session->set_flashdata('notif_msg', 'Delete '.$this->title.' failed');
 		}
 		redirect($this->redirect_url);
 	}
 
 	public function datatable(){
-		$this->lang->load('table', $this->session->userdata('lang'));
+		$indexs = $this->user->get_dt_table_field();
 
-		$this->user->dt_indexs 		= array('user.id', 'username', 'user.name', 'email', 'group.name as group_name', 'user.id');
-		$this->user->dt_join 		= array('group');
-		$this->user->dt_action_index = 5;
-		$this->user->dt_edit_action = true;
+		// if use join and 'as' in SELECT
+		$dt_join = array();
+		foreach ($indexs as $i => $index) {
+			if($index == 'group_id'){
+				$indexs[$i] = 'group.name as group_name';
+				$dt_join[] = 'group';
+			}elseif($index == 'id'){
+				$indexs[$i] = 'user.id';
+			}elseif($index == 'name'){
+				$indexs[$i] = 'user.name';
+			}
+		}
+
+		if(count($dt_join) > 0) $this->user->dt_join = $dt_join;
+
+		$this->user->dt_indexs        = $indexs;// array('gn_id', 'gn_name', "gn_id");
+		$this->user->dt_action_index  = (count($indexs) - 1);
+		$this->user->dt_edit_action   = true;
 		$this->user->dt_delete_action = true;
-		$this->user->dt_url_action 	= base_url().$this->config->item('index_page').'/admin/user/';
-		$this->user->dt_index_edit 	= 'id';
-		$this->user->dt_edit_label 	= lang('edit');
-		$this->user->dt_delete_label = lang('delete');
+		$this->user->dt_url_action    = base_url_admin().$this->controller.'/';
+		$this->user->dt_index_edit    = 'id';
+		// $this->user->dt_where       = array("user_id" => $this->session->userdata('id'));
 
 		echo $this->user->datatable();
+	}
+
+	public function print_excel(){
+		$this->load->library('excel');
+		$filename = 'Print Data '.$this->title;
+		$table_field = $this->user->table_field;
+
+		// if use join and 'as' in SELECT
+		$indexs = array();
+		foreach ($table_field as $i => $tf) {
+			if($tf['in_print'] == true){
+				$indexs[$i] = $tf['table_index'];
+				// in this case i want to change group_id to show group.name in excel
+				if($tf['table_index'] == 'group_id'){
+					$indexs[$i] = 'group.name as group_name';
+					$table_field[$i]['table_index'] = 'group_name';
+				}elseif($tf['table_index'] == 'id'){
+					$indexs[$i] = 'user.id';
+					// $table_field[$i]['table_index'] = 'group_name';
+				}elseif($tf['table_index'] == 'name'){
+					$indexs[$i] = 'user.name';
+					// $table_field[$i]['table_index'] = 'group_name';
+				}
+			}
+		}
+		$this->user->select(implode(', ', $indexs));
+		foreach ($this->user->belongs_to as $table => $join) {
+			$this->user->join($table);
+		}
+		// # end of // if use join and 'as' in SELECT
+		
+		$datas = $this->user->get_all();
+
+		$objPHPExcel = new PHPExcel();
+
+		// set properties
+		$objPHPExcel->getProperties()->setCreator($this->session->userdata('site_title'));
+		$objPHPExcel->getProperties()->setTitle($filename);
+		// $objPHPExcel->getProperties()->setCreator("ThinkPHP")
+		// 				->setLastModifiedBy("Daniel Schlichtholz")
+		// 				->setTitle("Office 2007 XLSX Test Document")
+		// 				->setSubject("Office 2007 XLSX Test Document")
+		// 				->setDescription("Test doc for Office 2007 XLSX, generated by PHPExcel.")
+		// 				->setKeywords("office 2007 openxml php")
+		// 				->setCategory("Test result file");
+		
+		$objWorksheet = $objPHPExcel->getActiveSheet();
+
+		$huruf = 'A';
+		$row = 1;
+		$count_cols = 0;
+		foreach ($table_field as $tf) {
+			if($tf['in_print'] == true){
+				$objWorksheet->getCell($huruf.$row)->setValue($tf['name']);
+				$huruf++;
+				$count_cols++;
+			}
+		}
+		$row++;
+		
+		// for date format
+		PHPExcel_Cell::setValueBinder( new PHPExcel_Cell_AdvancedValueBinder() );
+
+		foreach ($datas as $data) {
+			$huruf = 'A';
+			foreach ($table_field as $tf) {
+				if($tf['in_print'] == true){
+					$value = $data[$tf['table_index']];
+
+					$objWorksheet->getCell($huruf.$row)->setValue($value);
+					$objWorksheet->getColumnDimension($huruf)->setAutoSize(true);
+
+					if($tf['type'] == 'date' or $tf['type'] == 'datepicker'){
+						$objWorksheet->getStyle($huruf.$row)->getNumberFormat()->setFormatCode('dd/mm/yyyy');
+						// $objWorksheet->getStyle($huruf.$row)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_DATE_YYYYMMDDSLASH);
+						// $objWorksheet->getStyle($huruf.$row)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_DATE_DDMMYYYY);
+					}
+
+					$huruf++;
+				}
+			}
+			$row++;
+		}
+
+		// set border
+		// set the borderline
+		if($huruf == 'AA'){
+			$huruf = 'Z';
+		}elseif($huruf == 'AAA'){
+			$huruf = 'ZZ';
+		}elseif(strlen($huruf) == 2){
+			$first_huruf = substr($huruf, 0, 1);
+			$last_huruf = substr($huruf, 1, 1);
+			if($last_huruf == 'A'){
+				$first_huruf = chr(ord($first_huruf) - 1);
+				$last_huruf = 'Z';
+			}else{
+				$last_huruf = chr(ord($last_huruf) - 1);
+			}
+			$huruf = $first_huruf.$last_huruf;
+		}else{
+			$huruf = chr(ord($huruf) - 1);
+		}
+		$row--;
+		$objWorksheet->getStyle("A1:".$huruf.$row)->applyFromArray(
+			array(
+				'borders' => array(
+					'allborders' => array(
+						'style' => PHPExcel_Style_Border::BORDER_THIN,
+						// 'color' => array('rgb' => 'DDDDDD')
+					)
+				)
+			)
+		);
+
+		$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+
+		header('Content-type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+		header('Content-Disposition: attachment; filename="'.$filename.'.xlsx"');
+		$objWriter->save('php://output');
 	}
 }
 ?>
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,7 +1,7 @@
 <?php
 class User_model extends MY_Model {
 	protected $_table 		= 'user';
-	protected $primary_key = 'id';
+	public $primary_key 	= 'id';
 
 	// my style join
 	public $belongs_to = array('group' => array('from_column' => 'group_id', 'to_column' => 'id'));
@@ -10,5 +10,152 @@ class User_model extends MY_Model {
 	public $_user_id = 0; // from session 
 	public $before_create = array('create_mark');
 	public $before_update = array('update_mark');
+
+	public $table_field = array(
+		array(
+			// table param
+			'name' 			=> 'No',
+			'table_index'	=> 'id',
+			'style' 		=> 'width:15px;',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> false,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> false,
+			'type' 			=> 'hidden', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> false,
+			'maxlength' 	=> '',
+			// print param
+			'in_print'		=> true,
+		),
+		array(
+			// table param
+			'name' 			=> 'Name',
+			'table_index'	=> 'name',
+			'style' 		=> '',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> true,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> true,
+			'type' 			=> 'text', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> true,
+			'maxlength' 	=> '255',
+			// print param
+			'in_print'		=> true,
+		),
+		array(
+			// table param
+			'name' 			=> 'Username',
+			'table_index'	=> 'username',
+			'style' 		=> '',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> true,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> true,
+			'type' 			=> 'text', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> true,
+			'maxlength' 	=> '255',
+			// print param
+			'in_print'		=> true,
+		),
+		array(
+			// table param
+			'name' 			=> 'Password',
+			'table_index'	=> 'password',
+			'style' 		=> '',
+			'in_table' 		=> false,
+			// datatable param
+			'searchable' 	=> false,
+			'sortable' 		=> false,
+			// form param
+			'in_form' 		=> true,
+			'type' 			=> 'password', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> true,
+			'maxlength' 	=> '255',
+			// print param
+			'in_print'		=> false,
+		),
+		array(
+			// table param
+			'name' 			=> 'Confirm Password',
+			'table_index'	=> 'confirm_password',
+			'style' 		=> '',
+			'in_table' 		=> false,
+			// datatable param
+			'searchable' 	=> false,
+			'sortable' 		=> false,
+			// form param
+			'in_form' 		=> true,
+			'type' 			=> 'password', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> true,
+			'maxlength' 	=> '255',
+			// print param
+			'in_print'		=> false,
+		),
+		array(
+			// table param
+			'name' 			=> 'Email',
+			'table_index'	=> 'email',
+			'style' 		=> '',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> true,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> true,
+			'type' 			=> 'email', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> true,
+			'maxlength' 	=> '255',
+			// print param
+			'in_print'		=> true,
+		),
+		array(
+			// table param
+			'name' 			=> 'Group',
+			'table_index'	=> 'group_id',
+			'style' 		=> '',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> true,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> true,
+			'type' 			=> 'select', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> array('Group_model', 'id', 'name', array()),
+			'required' 		=> true,
+			'maxlength' 	=> '255',
+			// print param
+			'in_print'		=> true,
+		),
+		array(
+			// table param
+			'name' 			=> 'Action',
+			'table_index'	=> 'id',
+			'style' 		=> '', // 'width:15%;',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> false,
+			'sortable' 		=> false,
+			// form param
+			'in_form' 		=> false,
+			'type' 			=> 'hidden', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> false,
+			'maxlength' 	=> '',
+			// print param
+			'in_print'		=> false,
+		),
+	);
 }
 ?>
