e990be3c10467b23aaee51f05b373497ab19c763
Hanief <haniefhan@gmail.com>
2018-09-27 11:27:23 +0700

    menu management

---
+++ application/controllers/admin/Menu.php
@@ -0,0 +1,148 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Menu extends Admin_Controller implements ControllerInterface{
+	public function __construct(){
+		parent::__construct();
+		$this->load->model("Menu_model", 'menu');
+		$this->title = 'Menu';
+		$this->controller = 'menu';
+		$this->redirect_url = base_url_admin().'menu';
+	}
+
+	function index(){
+		$data = array();
+		$data['title']		= $this->title;
+		$data['controller']	= $this->controller;
+		$data['datas']		= array();
+		$data['datatable']	= base_url_admin().'menu/datatable';
+
+		// because it's simple index and form, use menu form
+		$data['table_field'] = $this->menu->table_field;
+		$data['content']	= 'content/common/index';
+
+		$this->template($data);
+	}
+
+	// add Page by form
+	function add(){
+		$data = array();
+		$data['title']		= 'Add New '.$this->title;
+		$data['controller']	= $this->controller;
+		$data['state']		= 'add';
+
+		// because it's simple index and form, use common form
+		$data['table_field'] = $this->menu->table_field;
+		foreach ($data['table_field'] as $i => $tf) {
+			if($tf['type'] == 'select'){
+				$this->load->model($tf['value'][0]);
+				$data['table_field'][$i]['value'] = $this->$tf['value'][0]->populate_select($tf['value'][1], $tf['value'][2], $tf['value'][3]);
+			}
+		}
+		$data['primary_key'] = $this->menu->primary_key;
+		$data['content']	= 'content/common/form';
+		$this->template($data);
+	}
+
+	// edit Page by form
+	function edit(){
+		$id = $this->input->get('id');
+		$data = array();
+		$data['title']		= 'Edit '.$this->title;
+		$data['controller']	= $this->controller;
+		$data['state']		= 'edit';
+		$data['id'] 		= $id;
+
+		// because it's simple index and form, use common form
+		$data['table_field'] = $this->menu->table_field;
+		foreach ($data['table_field'] as $i => $tf) {
+			if($tf['type'] == 'select'){
+				$this->load->model($tf['value'][0]);
+				$data['table_field'][$i]['value'] = $this->$tf['value'][0]->populate_select($tf['value'][1], $tf['value'][2], $tf['value'][3]);
+			}
+		}
+		$data['primary_key'] = $this->menu->primary_key;
+		$data['content']	= 'content/common/form';
+		$data['datas']      = $this->menu->get($id);
+
+		$data['datas'] = $this->menu->reformat_sql_to_form($data['datas']);
+
+		$this->template($data);
+	}
+
+	function insert(){
+		$this->db->trans_start();
+		$data = $this->input->post();
+
+		$data = $this->menu->reformat_post_to_sql($data);
+
+		if($this->menu->insert($data)){
+			$this->session->set_flashdata('notif_status', true);
+			$this->session->set_flashdata('notif_msg', 'Add new '.$this->title.' success');
+		}else{
+			$this->session->set_flashdata('notif_status', false);
+			$this->session->set_flashdata('notif_msg', 'Add new '.$this->title.' failed');
+		}
+
+		$this->db->trans_complete();
+		redirect($this->redirect_url);
+	}
+
+	function update(){
+		$this->db->trans_start();
+		$id = $this->input->get('id');
+		$data = $this->input->post();
+
+		$data = $this->menu->reformat_post_to_sql($data);
+
+		$st = $this->menu->update($id, $data);
+		if($st !== false){
+			$this->session->set_flashdata('notif_status', true);
+			$this->session->set_flashdata('notif_msg', 'Update '.$this->title.' success');
+		}else{
+			$this->session->set_flashdata('notif_status', false);
+			$this->session->set_flashdata('notif_msg', 'Update '.$this->title.' failed');
+		}
+		$this->db->trans_complete();
+
+		redirect($this->redirect_url);
+	}
+
+	function delete(){
+		$id = $this->input->get('id');
+
+		if($this->menu->delete($id)){
+			$this->session->set_flashdata('notif_status', true);
+			$this->session->set_flashdata('notif_msg', 'Delete '.$this->title.' success');
+		}else{
+			$this->session->set_flashdata('notif_status', false);
+			$this->session->set_flashdata('notif_msg', 'Delete '.$this->title.' failed');
+		}
+		redirect($this->redirect_url);
+	}
+
+	public function datatable(){
+		$indexs = $this->menu->get_dt_table_field();
+
+		// if use join and 'as' in SELECT
+		// $dt_join = array();
+		// foreach ($indexs as $i => $index) {
+		// 	if($index == 'parent'){
+		// 		$indexs[$i] = 'menu.parent as parent_name';
+		// 		$dt_join[] = 'menu';
+		// 	}
+		// }
+
+		// if(count($dt_join) > 0) $this->menu->dt_join = $dt_join;
+
+		$this->menu->dt_indexs        = $indexs;
+		$this->menu->dt_action_index  = (count($indexs) - 1);
+		$this->menu->dt_edit_action   = true;
+		$this->menu->dt_delete_action = true;
+		$this->menu->dt_url_action    = base_url_admin().$this->controller.'/';
+		$this->menu->dt_index_edit    = 'id';
+		// $this->menu->dt_where       = array("user_id" => $this->session->userdata('id'));
+		
+		echo $this->menu->datatable();
+	}
+}
+?>
--- application/models/Menu_model.php
+++ application/models/Menu_model.php
@@ -1,7 +1,159 @@
 <?php
 class Menu_model extends MY_Model {
 	protected $_table 		= 'menu';
-	protected $primary_key = 'id';
+	public $primary_key = 'id';
+
+	public $belongs_to = array('menu' => array('from_column' => 'parent', 'to_column' => 'id'));
+
+	public $before_create = array('create_mark');
+	public $before_update = array('update_mark');
+
+	public $table_field = array(
+		array(
+			// table param
+			'name' 			=> 'No',
+			'table_index'	=> 'id',
+			'style' 		=> 'width:15px;',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> false,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> false,
+			'type' 			=> 'hidden', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> false,
+			'maxlength' 	=> '',
+			// print param
+			'in_print'		=> true,
+		),
+		array(
+			// table param
+			'name' 			=> 'Name',
+			'table_index'	=> 'name',
+			'style' 		=> '',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> true,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> true,
+			'type' 			=> 'text', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> true,
+			'maxlength' 	=> '255',
+			// print param
+			'in_print'		=> true,
+		),
+		array(
+			// table param
+			'name' 			=> 'URL',
+			'table_index'	=> 'url',
+			'style' 		=> '',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> true,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> true,
+			'type' 			=> 'text', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> true,
+			'maxlength' 	=> '255',
+			// print param
+			'in_print'		=> true,
+		),
+		array(
+			// table param
+			'name' 			=> 'Icon',
+			'table_index'	=> 'icon',
+			'style' 		=> '',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> true,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> true,
+			'type' 			=> 'text', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> true,
+			'maxlength' 	=> '255',
+			// print param
+			'in_print'		=> true,
+		),
+		array(
+			// table param
+			'name' 			=> 'Status',
+			'table_index'	=> 'status',
+			'style' 		=> '',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> true,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> true,
+			'type' 			=> 'numeric', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> 1,
+			'required' 		=> true,
+			'maxlength' 	=> 1,
+			// print param
+			'in_print'		=> true,
+		),
+		array(
+			// table param
+			'name' 			=> 'Order',
+			'table_index'	=> 'order',
+			'style' 		=> '',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> true,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> true,
+			'type' 			=> 'numeric', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> 1,
+			'required' 		=> true,
+			'maxlength' 	=> 2,
+			// print param
+			'in_print'		=> true,
+		),
+		array(
+			// table param
+			'name' 			=> 'Parent',
+			'table_index'	=> 'parent',
+			'style' 		=> '',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> true,
+			'sortable' 		=> true,
+			// form param
+			'in_form' 		=> true,
+			'type' 			=> 'select', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> array('Menu_model', 'id', 'name', array()),
+			'required' 		=> true,
+			'maxlength' 	=> 2,
+			// print param
+			'in_print'		=> true,
+		),
+		array(
+			// table param
+			'name' 			=> 'Action',
+			'table_index'	=> 'id',
+			'style' 		=> '', // 'width:15%;',
+			'in_table' 		=> true,
+			// datatable param
+			'searchable' 	=> false,
+			'sortable' 		=> false,
+			// form param
+			'in_form' 		=> false,
+			'type' 			=> 'hidden', // hidden, text, select, textarea, date, datepicker, numeric, money
+			'value' 		=> '',
+			'required' 		=> false,
+			'maxlength' 	=> '',
+			// print param
+			'in_print'		=> false,
+		),
+	);
 
 	public function menu_structured(){
 		$res = $this->get_all();
@@ -38,6 +190,72 @@ class Menu_model extends MY_Model {
 
 	public function access_name(){
 		return array('view', 'add', 'edit', 'delete');
+	}
+
+	public function populate_select($index_field = '', $value_field = '', $where = array(), $join = array()){
+	    $ret = array();
+	    $menu2 = array();
+	    $where1 = $where;
+	    $where1['parent !='] = 0;
+
+	    $select = array();
+	    $select[] = $index_field;
+	    $select[] = 'parent';
+	    if(is_array($value_field)){
+	        foreach ($value_field as $vf) {
+	            $select[] = $vf;
+	        }
+	    }else{
+	        $select[] = $value_field;
+	    }
+	    $this->select(implode(', ', $select));
+	    if(count($join) > 0){
+	        foreach ($join as $j) {
+	            $this->join($j);   
+	        }
+	    }
+	    $this->order_by('order', 'ASC');
+
+	    foreach ($this->get_many_by($where1) as $data) {
+	        if(is_array($value_field)){
+	            $menu2[$data['parent']][$data['id']] = '  -- ';
+	            foreach ($value_field as $vf) {
+	                if($menu2[$data['parent']][$data['id']] != '') $menu2[$data['parent']][$data['id']] .= ' - ';
+	                $menu2[$data['parent']][$data['id']] .= $data[$vf];
+	            }
+	        }else{
+	            $menu2[$data['parent']][$data['id']] = '  -- '.$data[$value_field];
+	        }
+	    }
+
+	    $this->select(implode(', ', $select));
+	    if(count($join) > 0){
+	        foreach ($join as $j) {
+	            $this->join($j);   
+	        }
+	    }
+	    $this->order_by('order', 'ASC');
+	    $where['parent'] = 0;
+
+	    foreach ($this->get_many_by($where) as $data) {
+	        if(is_array($value_field)){
+	            $ret[$data[$index_field]] = '';
+	            foreach ($value_field as $vf) {
+	                if($ret[$data[$index_field]] != '') $ret[$data[$index_field]] .= ' - ';
+	                $ret[$data[$index_field]] .= $data[$vf];
+	            }
+	        }else{
+	            $ret[$data[$index_field]] = $data[$value_field];
+	        }
+
+	        if(isset($menu2[$data[$index_field]])){
+	        	foreach ($menu2[$data[$index_field]] as $index => $value) {
+	        		$ret[$index] = $value;
+	        	}
+	        }
+	    }
+
+	    return $ret;
 	}
 }
 ?>
