65a4822174d73bcb4ea6e72c5ac8c44007ee3a92
Hanief <haniefhan@gmail.com>
2018-12-20 10:00:08 +0700

    access :: access with datatable and fix left

--- application/views/template/adminlte/content/access/index.php
+++ application/views/template/adminlte/content/access/index.php
@@ -1,5 +1,5 @@
 <div class="row">
-	<div class="col-lg-9 col-md-10 col-sm-12 col-xs-12">
+	<div class="col-lg-10 col-md-12 col-sm-12 col-xs-12">
 		<div class="box box-primary">
 			<div class="box-body">
 				<form role="form" class="form-horizontal" action="<?php echo base_url_admin() ?>access/edit" method="GET">
@@ -26,95 +26,98 @@
 	<?php // $this->load->view($this->session->userdata('template_admin_use').'notification'); ?>
 	<!-- #Notification -->
 	<?php if(isset($menus)){ ?>
-		<div class="col-lg-9 col-md-12 col-sm-12 col-xs-12">
+		<div class="col-lg-10 col-md-12 col-sm-12 col-xs-12">
 			<form role="form" class="form-horizontal" action="<?php echo base_url_admin() ?>access/update?id=<?php echo $id ?>" method="POST">
 				<input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>" />
 				<div class="box box-primary">
 					<div class="box-header with-border"><h3 class="box-title">Set Access Right</h3></div>
 					<div class="box-body">
-						<input type="submit" class="btn btn-primary pull-right" value="Save Access">
-						<br/>
-						<br/>
-						<table class="table table-striped table-bordered table-hover">
-							<thead>
-								<tr>
-									<th style="width: 5%;">No</th>
-									<th>Menu</th>
-									<?php foreach ($access_name as $access) {?>
-										<th style="width: 5%;" class="text-center"><?php echo ucfirst(str_replace('_', ' ', $access)); ?></th>
-									<?php } ?>
-								</tr>
-							</thead>
-							<tbody>
-								<?php $no = 1; foreach ($menus as $i => $menu) {?>
-									<?php 
-										$access_manage = json_decode($menu['access_manage'], true);
-										if(!is_array($access_manage)) $access_manage = array();
-									?>
+						<!-- <div class="table-responsive"> -->
+							<!-- <input type="submit" class="btn btn-primary pull-right" value="Save Access">
+							<br/>
+							<br/> -->
+							<table class="table table-striped table-bordered table-hover" id="table-access">
+								<thead>
 									<tr>
-										<td><?php echo $no; $no++; ?>.</td>
-										<td><?php echo $menu['name'] ?> <input type="checkbox" class="verticalCheck childCheck pull-right" id="<?php echo $i ?>" title="Select All Right and Child"></td>
+										<!-- <th>#</th> -->
+										<th style="width: 5%;">No</th>
+										<th style="width: 210px;">Menu</th>
 										<?php foreach ($access_name as $access) {?>
-											<td class="text-center">
-												<?php if(in_array($access, $access_manage)){ ?>
-													<?php $sel = ''; if(isset($datas[$menu['id']][$access])){ if($datas[$menu['id']][$access] == 1) $sel = 'checked'; } ?>
-													<input type="checkbox" name="menus[<?php echo $menu['id'] ?>][]" value="<?php echo $access ?>" <?php echo $sel ?>>
-												<?php } ?>
-											</td>
+											<th class="text-center"><?php echo ucfirst(str_replace('_', ' ', $access)); ?></th>
 										<?php } ?>
 									</tr>
-									<?php if(isset($menu['children'])){ ?>
-										<?php foreach ($menu['children'] as $j => $menu2) {?>
-											<?php 
-												$access_manage = json_decode($menu2['access_manage'], true);
-												if(!is_array($access_manage)) $access_manage = array();
-											?>
-											<tr>
-												<td></td>
-												<td>-- <?php echo $menu2['name']; ?></td>
-												<?php foreach ($access_name as $access) { ?>
-													<td class="text-center">
-														<?php if(in_array($access, $access_manage)){ ?>
-															<?php $sel = ''; if(isset($datas[$menu2['id']][$access])){ if($datas[$menu2['id']][$access] == 1) $sel = 'checked'; } ?>
-															<input type="checkbox" class="child-<?php echo $i ?>" name="menus[<?php echo $menu2['id'] ?>][]" value="<?php echo $access ?>" <?php echo $sel ?>>
-														<?php } ?>
-													</td>
-												<?php } ?>
-											</tr>
-											<?php foreach ($menu2['children'] as $j => $menu3) {?>
+								</thead>
+								<tbody>
+									<?php $no_all = 1; $no = 1; foreach ($menus as $i => $menu) {?>
+										<?php 
+											$access_manage = json_decode($menu['access_manage'], true);
+											if(!is_array($access_manage)) $access_manage = array();
+										?>
+										<tr>
+											<td><?php echo $no; $no++; ?>.</td>
+											<td><?php echo $menu['name'] ?> <input type="checkbox" class="verticalCheck childCheck pull-right" id="<?php echo $i; ?>" title="Select All Right and Child"></td>
+											<?php foreach ($access_name as $access) {?>
+												<td class="text-center">
+													<?php if(in_array($access, $access_manage)){ ?>
+														<?php $sel = ''; if(isset($datas[$menu['id']][$access])){ if($datas[$menu['id']][$access] == 1) $sel = 'checked'; } ?>
+														<input type="checkbox" name="menus[<?php echo $menu['id']; ?>][]" value="<?php echo $access ?>" <?php echo $sel ?>>
+													<?php } ?>
+												</td>
+											<?php } ?>
+										</tr>
+										<?php if(isset($menu['children'])){ ?>
+											<?php foreach ($menu['children'] as $j => $menu2) {?>
 												<?php 
-													$access_manage = json_decode($menu3['access_manage'], true);
+													$access_manage = json_decode($menu2['access_manage'], true);
 													if(!is_array($access_manage)) $access_manage = array();
 												?>
 												<tr>
 													<td></td>
-													<td>&nbsp;&nbsp;&nbsp;&nbsp;-- <?php echo $menu3['name']; ?></td>
+													<td>-- <?php echo $menu2['name']; ?> <input type="checkbox" class="verticalCheck childCheck pull-right child-<?php echo $i ?>" id="<?php echo $i.'.'.$j; ?>" title="Select All Right and Child"></td>
 													<?php foreach ($access_name as $access) { ?>
 														<td class="text-center">
 															<?php if(in_array($access, $access_manage)){ ?>
-																<?php $sel = ''; if(isset($datas[$menu3['id']][$access])){ if($datas[$menu3['id']][$access] == 1) $sel = 'checked'; } ?>
-																<input type="checkbox" class="child-<?php echo $i ?>" name="menus[<?php echo $menu3['id'] ?>][]" value="<?php echo $access ?>" <?php echo $sel ?>>
+																<?php $sel = ''; if(isset($datas[$menu2['id']][$access])){ if($datas[$menu2['id']][$access] == 1) $sel = 'checked'; } ?>
+																<input type="checkbox" class="child-<?php echo $i ?> child-<?php echo $i.'.'.$j; ?>" name="menus[<?php echo $menu2['id'] ?>][]" value="<?php echo $access ?>" <?php echo $sel ?>>
 															<?php } ?>
 														</td>
 													<?php } ?>
 												</tr>
+												<?php foreach ($menu2['children'] as $k => $menu3) {?>
+													<?php 
+														$access_manage = json_decode($menu3['access_manage'], true);
+														if(!is_array($access_manage)) $access_manage = array();
+													?>
+													<tr>
+														<td></td>
+														<td>&nbsp;&nbsp;&nbsp;&nbsp;-- <?php echo $menu3['name']; ?> <input type="checkbox" class="verticalCheck childCheck pull-right" id="<?php echo $i.'.'.$j.'.'.$k; ?>" title="Select All Right and Child child-<?php echo $i.'.'.$j; ?>"></td>
+														<?php foreach ($access_name as $access) { ?>
+															<td class="text-center">
+																<?php if(in_array($access, $access_manage)){ ?>
+																	<?php $sel = ''; if(isset($datas[$menu3['id']][$access])){ if($datas[$menu3['id']][$access] == 1) $sel = 'checked'; } ?>
+																	<input type="checkbox" class="child-<?php echo $i ?> child-<?php echo $i.'.'.$j ?> child-<?php echo $i.'.'.$j.'.'.$k; ?>" name="menus[<?php echo $menu3['id'] ?>][]" value="<?php echo $access ?>" <?php echo $sel ?>>
+																<?php } ?>
+															</td>
+														<?php } ?>
+													</tr>
+												<?php } ?>
 											<?php } ?>
 										<?php } ?>
 									<?php } ?>
-								<?php } ?>
-							</tbody>
-							<tfoot>
-								<tr>
-									<th style="width: 5%;">No</th>
-									<th>Menu</th>
-									<?php foreach ($access_name as $access) {?>
-										<th style="width: 5%;" class="text-center"><?php echo ucfirst(str_replace('_', ' ', $access)); ?></th>
-									<?php } ?>
-								</tr>
-							</tfoot>
-						</table>
-						<br/>
-						<input type="submit" class="btn btn-primary pull-right" value="Save Access">
+								</tbody>
+								<!-- <tfoot>
+									<tr>
+										<th style="width: 5%;">No</th>
+										<th>Menu</th>
+										<?php foreach ($access_name as $access) {?>
+											<th style="width: 5%;" class="text-center"><?php echo ucfirst(str_replace('_', ' ', $access)); ?></th>
+										<?php } ?>
+									</tr>
+								</tfoot> -->
+							</table>
+							<br/>
+							<input type="submit" class="btn btn-primary pull-right" value="Save Access">
+						<!-- </div> -->
 					</div>
 				</div>
 			</form>
--- application/views/template/adminlte/content/access/script.php
+++ application/views/template/adminlte/content/access/script.php
@@ -1,3 +1,6 @@
+<script type="text/javascript" src="<?php echo asset_admin_url() ?>bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="<?php echo asset_admin_url() ?>bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo asset_admin_url() ?>bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
 <script type="text/javascript">
 	$(function(){
 		$('.verticalCheck').click(function(){
@@ -11,5 +14,18 @@
 
 			$(this).parent().parent().parent().find(':checkbox.child-'+id).prop('checked', check);
 		})
+
+		$('table#table-access').DataTable({
+			scrollY : "400px",
+			scrollX : true,
+			scrollCollapse : true,
+			paging : false,
+			searching : false,
+			ordering : false,
+			bInfo : false,
+			fixedColumns:   {
+				leftColumns: 1,
+			}
+		});
 	})
 </script>
