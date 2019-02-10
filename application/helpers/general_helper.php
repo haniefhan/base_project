2bd80eefee863cbbe3430bdd346fabe844429d6e
Hanief <haniefhan@gmail.com>
2019-02-10 20:26:20 +0700

    helper :: add function for count days from two date, money format, and terbilang for spelling number in bahasa indonesia

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -250,4 +250,61 @@ if ( ! function_exists('time_format_ext')){
 		return $date;
 	}
 }
+
+if ( ! function_exists('day_two_dates')){
+	function day_two_dates($to = '', $from = '') {
+		if($from == '') $date_from = now();
+		else $date_from = strtotime($from);
+		$date_to = strtotime($to);
+		$datediff = $date_to - $date_from;
+
+		return round($datediff / (60 * 60 * 24));
+	}
+}
+
+if ( ! function_exists('money_format_ext')){
+	// source : http://php.net/manual/en/function.strftime.php
+	function money_format_ext($money = 0, $suffix = 2) {
+		return number_format($money, $suffix, ',', '.');
+	}
+}
+
+if ( ! function_exists('terbilang')){
+	function penyebut($nilai) {
+		$nilai = abs($nilai);
+		$huruf = array("", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas");
+		$temp = "";
+		if ($nilai < 12) {
+			$temp = " ". $huruf[$nilai];
+		} else if ($nilai < 20) {
+			$temp = penyebut($nilai - 10). " belas";
+		} else if ($nilai < 100) {
+			$temp = penyebut($nilai/10)." puluh". penyebut($nilai % 10);
+		} else if ($nilai < 200) {
+			$temp = " seratus" . penyebut($nilai - 100);
+		} else if ($nilai < 1000) {
+			$temp = penyebut($nilai/100) . " ratus" . penyebut($nilai % 100);
+		} else if ($nilai < 2000) {
+			$temp = " seribu" . penyebut($nilai - 1000);
+		} else if ($nilai < 1000000) {
+			$temp = penyebut($nilai / 1000) . " ribu" . penyebut($nilai % 1000);
+		} else if ($nilai < 1000000000) {
+			$temp = penyebut($nilai/1000000) . " juta" . penyebut($nilai % 1000000);
+		} else if ($nilai < 1000000000000) {
+			$temp = penyebut($nilai/1000000000) . " milyar" . penyebut(fmod($nilai,1000000000));
+		} else if ($nilai < 1000000000000000) {
+			$temp = penyebut($nilai/1000000000000) . " trilyun" . penyebut(fmod($nilai,1000000000000));
+		}
+		return $temp;
+	}
+	
+	function terbilang($nilai) {
+		if($nilai<0) {
+			$hasil = "minus ". trim(penyebut($nilai));
+		} else {
+			$hasil = trim(penyebut($nilai));
+		}
+		return $hasil;	
+	}
+}
 ?>
